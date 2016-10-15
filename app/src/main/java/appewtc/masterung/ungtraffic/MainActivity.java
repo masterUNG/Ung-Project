336f8483962e8ed471e9aba50cfa956d523e0b7c
masterUNG <phrombutr@gmail.com>
2016-10-15 15:00:54 +0700

    finish 15oct

--- app/src/main/java/appewtc/masterung/ungtraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/ungtraffic/MainActivity.java
@@ -1,14 +1,51 @@
 package appewtc.masterung.ungtraffic;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit การประกาศตัวแปร
+    private ListView listView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        //Bind  Widget
+        listView = (ListView) findViewById(R.id.livTraffic);
+        button = (Button) findViewById(R.id.button);
+
+        //Button Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),
+                        R.raw.mosquito);
+                mediaPlayer.start();
+
+                //Show WebView
+                String url = "https://youtu.be/AFmWqLIqDZA";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+
+            }   // onClick
+        });
+
+
+    }   // Main Method
+
 }   // Main Class นี่คือ คลาสหลัก
 
